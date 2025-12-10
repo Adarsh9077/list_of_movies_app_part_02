b47d9b7f82420786fc9bd940cd9c9e7e0d8c3ac1
Adarsh9077 <bhardwajadarsh873@gmail.com>
2025-12-10 23:51:38 +0530

    add delete all favorite movies from list

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:mvvm_state_management/constants/my_app_icons.dart';
+import 'package:mvvm_state_management/view_models/favorite_provider.dart';
 import 'package:mvvm_state_management/widgets/movies/movie_widget.dart';
+import 'package:provider/provider.dart';
 
 class FavoritesScreen extends StatefulWidget {
   const FavoritesScreen({super.key});
@@ -11,27 +13,49 @@ class FavoritesScreen extends StatefulWidget {
 class _FavoritesScreenState extends State<FavoritesScreen> {
   @override
   Widget build(BuildContext context) {
+    final favoriteProvider = Provider.of<FavoriteProvider>(
+      context,
+      listen: false,
+    );
     return Scaffold(
       appBar: AppBar(
         title: Text("Favorites"),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              favoriteProvider.clearAllFavorite();
+            },
             icon: Icon(MyAppIcons.delete, color: Colors.red),
           ),
         ],
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: ListView.builder(
-          itemCount: 100,
-          itemBuilder: (context, index) {
-            return MovieWidget(); //Text("data");
-            //  MovieWidget()
-          },
-        ),
-      ),
+      body: favoriteProvider.favoritesList.isEmpty
+          ? const Center(
+              child: Text(
+                "No add Favorites",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+            )
+          : Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: ListView.builder(
+                itemCount: favoriteProvider.favoritesList.length,
+                itemBuilder: (context, index) {
+                  return Builder(
+                    builder: (context) {
+                      return ChangeNotifierProvider.value(
+                        value: favoriteProvider.favoritesList.reversed
+                            .toList()[index],
+                        child: MovieWidget(),
+                      );
+                    },
+                  ); //Text("data");
+                  //  MovieWidget()
+                },
+              ),
+            ),
     );
   }
 }
+// 02:45
